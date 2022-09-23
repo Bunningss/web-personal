6aa6151b3a92400b9b75024d449ddddf8ae6b244
Tayef <mahmudovi183@gmail.com>
2022-09-23 15:46:36 +0600

    api-call-test

--- reqMethod.js
+++ reqMethod.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const base_URL = 'http://localhost:3000/api/'
+const base_URL = process.env.baseURL || 'https://tmahmud.herokuapp.com/api/'
 
 export const publicCall = axios.create({
     baseURL: base_URL
