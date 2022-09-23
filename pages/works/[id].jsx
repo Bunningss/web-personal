6d9117a4b4eeffe8073237f2cc10bd41cf52bfac
Tayef <mahmudovi183@gmail.com>
2022-09-23 11:26:55 +0600

    rr.

--- pages/works/[id].jsx
+++ pages/works/[id].jsx
@@ -14,7 +14,7 @@ const Work = () => {
         <div className={h > 10 ? `${styles.cover_image} ${styles.slide}` : `${styles.cover_image}`}>
           <Image src={demo} alt='' layout='fill' objectFit='cover' loading="lazy"/>
         </div>
-        <p className={`text-regular ${styles.text}`}>
+        <p className={`text-regular-small ${styles.text}`}>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis aut hic corporis voluptatum rerum vel dolorem non dignissimos laudantium vero dolores magni totam quae, delectus, fugiat in nostrum consectetur harum! Temporibus in laudantium quaerat fugiat quam praesentium vitae unde illum vero ex corporis tempora ad iusto sunt beatae, alias ea?
         </p>
       </div>
