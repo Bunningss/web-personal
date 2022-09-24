60cdccf9730b095e4d5e176f70ee5f6d30be1395
Tayef <mahmudovi183@gmail.com>
2022-09-24 13:16:01 +0600

    test

--- reqMethod.js
+++ reqMethod.js
@@ -1,6 +1,6 @@
-import axios from 'axios'
+import axios from 'axios';
 
-const base_URL = process.env.baseURL || 'https://tmahmud.herokuapp.com/api/'
+const base_URL = process.env.baseURL
 
 export const publicCall = axios.create({
     baseURL: base_URL
